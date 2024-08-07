66d3da193d5176f24bce6322e4546e860a97683b
FlashScript <itzflashscript@gmail.com>
2024-08-07 21:17:11 +0330

    Finished the Mobile Design

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -4,7 +4,7 @@ import { cardDescription, cardPublishedTime, cardTag, cardTitle, cardUserName } 
 const BlogCard = () => {
 
     return (
-        <div className={`max-w-[337px] p-6 relative | bg-neutral-white font-figtree | border border-neutral-black rounded-3xl | `}>
+        <div className={`max-w-[330px] p-6 relative | bg-neutral-white font-figtree | border border-neutral-black rounded-3xl | `}>
             <img 
                 src={illustrationImage} 
                 alt="Illustration Image" 
@@ -19,12 +19,12 @@ const BlogCard = () => {
                 {cardPublishedTime}
             </h6>
 
-            <a className={` |  |  | group`} href="#">
-                <div className={`absolute w-[101%] h-[101%] top-0 left-10 -z-10 bg-neutral-black |  | rounded-3xl | group`} />
-
-                <h3 className={`mb-3 | text-[1.3rem] text-neutral-black font-extrabold |  | `}>
+            <a className={`inline-block |  |  | group`} href="#">
+                <h3 className={`mb-3 | text-[1.3rem] text-neutral-black hover:text-primary-yellow font-extrabold transition-all |  | group`}>
                     {cardTitle}
                 </h3>
+                
+                <div className={`absolute w-[101%] h-[101%] inset-0 -z-10 left-[0.5rem] top-[0.4rem] | bg-neutral-black | rounded-3xl | group-hover:left-[0.8rem] group-hover:top-[0.8rem] transition-all`} />
             </a>
 
             <p className={`mb-6 | text-[0.9rem] text-neutral-grey font-semibold |  | `}>
