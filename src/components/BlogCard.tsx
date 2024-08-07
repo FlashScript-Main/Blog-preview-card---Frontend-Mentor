f844cfe0e6d96758bac54c7be84a5a58d6dd5bb7
FlashScript <itzflashscript@gmail.com>
2024-08-07 21:26:43 +0330

    Finished the Desktop Design

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -4,30 +4,30 @@ import { cardDescription, cardPublishedTime, cardTag, cardTitle, cardUserName } 
 const BlogCard = () => {
 
     return (
-        <div className={`max-w-[330px] p-6 relative | bg-neutral-white font-figtree | border border-neutral-black rounded-3xl | `}>
+        <div className={`max-w-[330px] md:max-w-[380px] p-6 relative | bg-neutral-white font-figtree | border border-neutral-black rounded-3xl | `}>
             <img 
                 src={illustrationImage} 
                 alt="Illustration Image" 
-                className={`mb-6 |  | rounded-xl | `}
+                className={`mb-6 md:mb-7 |  | rounded-xl | `}
             />
 
-            <span className={`px-3 py-[0.3rem] | text-sm text-neutral-black bg-primary-yellow font-extrabold rounded-[0.25rem] |  | `}>
+            <span className={`px-3 py-[0.3rem] md:py-[0.35rem] | text-sm text-neutral-black bg-primary-yellow font-extrabold rounded-[0.25rem] |  | `}>
                 {cardTag}
             </span>
 
-            <h6 className={`mt-4 mb-3 | text-neutral-black text-[0.8rem] font-semibold |  | `}>
+            <h6 className={`mt-4 mb-3 | text-neutral-black text-[0.8rem] md:text-sm font-semibold |  | `}>
                 {cardPublishedTime}
             </h6>
 
             <a className={`inline-block |  |  | group`} href="#">
-                <h3 className={`mb-3 | text-[1.3rem] text-neutral-black hover:text-primary-yellow font-extrabold transition-all |  | group`}>
+                <h3 className={`mb-3 | text-[1.3rem] md:text-2xl text-neutral-black hover:text-primary-yellow font-extrabold transition-all |  | group`}>
                     {cardTitle}
                 </h3>
                 
-                <div className={`absolute w-[101%] h-[101%] inset-0 -z-10 left-[0.5rem] top-[0.4rem] | bg-neutral-black | rounded-3xl | group-hover:left-[0.8rem] group-hover:top-[0.8rem] transition-all`} />
+                <div className={`absolute w-[101%] h-[101%] inset-0 -z-10 left-[0.3rem] top-[0.3rem] | bg-neutral-black | rounded-3xl | group-hover:left-[0.8rem] group-hover:top-[0.8rem] transition-all`} />
             </a>
 
-            <p className={`mb-6 | text-[0.9rem] text-neutral-grey font-semibold |  | `}>
+            <p className={`mb-6 | text-[0.9rem] md:text-[0.97rem] text-neutral-grey font-semibold |  | `}>
                 {cardDescription}
             </p>
 
